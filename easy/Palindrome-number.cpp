4c5f535de6beb74487482733a61c5722745d0c39
yntcdd <96919146+yntcdd@users.noreply.github.com>
2025-10-15 22:36:54 -0400

    Add Palindrome-number solution class

---
+++ easy/Palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        long rev = 0;
+
+        while (x != 0) {
+            rev=(rev*10)+(x%10);
+            x=x/10;
+        }
+
+        return temp==rev;
+    }
+};
